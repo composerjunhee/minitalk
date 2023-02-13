b962cc5762669f6feb765e200ca0b10e8347c007
Kimpozer <composerjunhee@gmail.com>
2023-02-13 18:50:22 +0200

    client update

--- client.c
+++ client.c
@@ -6,21 +6,68 @@
 /*   By: junheeki <junheeki@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 14:25:56 by junheeki          #+#    #+#             */
-/*   Updated: 2023/02/13 17:21:18 by junheeki         ###   ########.fr       */
+/*   Updated: 2023/02/13 18:46:14 by junheeki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-int main(int argc, char **argv)
+static void	action(int sig)
+{
+	static int	received;
+
+	received = 0;
+	if (sig == SIGUSR1)
+		++received;
+	else
+	{
+		ft_putnbr_fd(received, 1);
+		ft_putchar_fd('\n', 1);
+		exit(0);
+	}
+}
+
+static void	mt_kill(int pid, char *str)
+{
+	int	i;
+	char	c;
+
+	while(*str)
+	{
+		i = 8;
+		c = *str++;
+		while (i--)
+		{
+			if (c >> i & 1)
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+			usleep(100);
+		}
+	}
+	i = 8;
+	while (i--)
+	{
+		kill(pid, SIGUSR1);
+		usleep(100);
+	}
+}
+
+int	main(int argc, char **argv)
 {
 	if (argv != 3 || !ft_strlen(argv[2]))
 		return (1);
-	struct sigaction sa;
-
-
+	ft_putstr_fd("Sent	: ", 1);
+	ft_putnbr_fd(ft_strlen(argv[2], 1));
+	ft_putchar_fd('\n', 1);
+	ft_putstr_fd("Received: ", 1);
+	signal(SIGUSR1, action);
+	signal(SIGUSR2, action);
+	mt_kill(ft_atoi(argv[1]), argv[2]);
+	while (1)
+		pause();
+	return (0);
 }
--- server.c
+++ server.c
@@ -6,31 +6,28 @@
 /*   By: junheeki <junheeki@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 14:25:56 by junheeki          #+#    #+#             */
-/*   Updated: 2023/02/13 18:18:07 by junheeki         ###   ########.fr       */
+/*   Updated: 2023/02/13 18:41:27 by junheeki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <stdio.h>
 #include "libft/libft.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-static void action(int sig, siginfo_t *info, void *context)
+static void	action(int sig, siginfo_t *info, void *context)
 {
-
 }
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-	struct sigaction sa;
+	struct sigaction	sa;
 
 	ft_putstr_fd("Server PID: ", 1);
 	ft_putnbr_fd(getpid(), 1);
 	ft_putchar_fd('\n', 1);
 	sa.sa_sigaction = action;
 	sa.sa_flags = SA_SIGINFO;
-	asdfa
-	return 0;
+	return (0);
 }
